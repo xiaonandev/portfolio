28542da2072ca84d5d566951382fb98461d98ff7
xiaonan <14mingyue@gmail.com>
2026-07-24 17:58:58 +0200

    fix: fix broken image

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -1,15 +1,15 @@
-import SectionHeading from '@/components/Helper/SectionHeading';
-import Image from 'next/image';
-import { useTranslations } from 'next-intl';
+import SectionHeading from "@/components/Helper/SectionHeading";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 const About = () => {
-  const t = useTranslations('About');
+  const t = useTranslations("About");
   return (
     <div className="py-16 bg-gray-100 dark:bg-gray-900">
       <section id="about">
         <SectionHeading
-          title_1={t('title1')}
-          title_2={t('title2')}
+          title_1={t("title1")}
+          title_2={t("title2")}
           description=""
         />
 
@@ -17,17 +17,17 @@ const About = () => {
           <div className="relative">
             <div className="aspect-square rounded-2xl overflow-hidden p-2">
               <Image
-                src={'/images/profile.jpg'}
-                alt="profile"
+                src={"/images/selfie-1.jpg"}
+                alt="selfie"
                 width={1238}
                 height={1280}
-                className="w-full h-full object-center rounded-xl"
+                className="w-full h-full object-cover rounded-xl"
               />
             </div>
           </div>
           <div className="space-y-6">
             <p className="text-muted-foreground leading-relaxed whitespace-pre-line">
-              {t('description')}
+              {t("description")}
             </p>
           </div>
         </div>
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,5 +2,5 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "nl", "ja"],
-  defaultLocale: "nl",
+  defaultLocale: "en",
 });
